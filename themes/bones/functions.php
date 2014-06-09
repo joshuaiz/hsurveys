cdcb70ae73d618dd9d691cadc683934c70ba3891
joshuaiz <joshuaiz@me.com>
2014-06-09 18:52:56 -0500

    deploy test

--- themes/bones/functions.php
+++ themes/bones/functions.php
@@ -419,5 +419,37 @@ class GFLimitCheckboxes {
 //     ));
 
 
+function wp_browser_body_class($classes) {
+global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone, $is_ipad;
+if($is_lynx) $classes[] = 'lynx';
+elseif($is_gecko) $classes[] = 'gecko';
+elseif($is_opera) $classes[] = 'opera';
+elseif($is_NS4) $classes[] = 'ns4';
+elseif($is_safari) $classes[] = 'safari';
+elseif($is_chrome) $classes[] = 'chrome';
+elseif($is_IE) {
+$classes[] = 'ie';
+if(preg_match('/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version))
+$classes[] = 'ie'.$browser_version[1];
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== -1) {
+            $classes[] = 'ie11';
+            $classes[] = 'ie';
+} else $classes[] = 'unknown';
+if($is_iphone) $classes[] = 'iphone';
+if($is_ipad) $classes[] = 'ipad';
+if (strstr($_SERVER['HTTP_USER_AGENT'], 'iPad')) {
+   $classes[] = 'ipad';
+}
+if ( stristr( $_SERVER['HTTP_USER_AGENT'],"mac") ) {
+$classes[] = 'osx';
+} elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"linux") ) {
+$classes[] = 'linux';
+} elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"windows") ) {
+$classes[] = 'windows';
+}
+return $classes;
+}
+add_filter('body_class','wp_browser_body_class');
+
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
